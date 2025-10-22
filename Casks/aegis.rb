d0ffbf140bdf192be4b763ad94986629879d202d
Khalid Alkhaldi <k.t.alkhaldi@gmail.com>
2025-10-22 16:45:30 -0500

    Update aegis.rb

--- Casks/aegis.rb
+++ Casks/aegis.rb
@@ -1,8 +1,7 @@
 cask 'aegis' do
   version '1.0.0'
   sha256 'a552e16540d539387c730f07e098c9fde31b383d79202af4a15f503273f1bc31'
-
-  url 'https://github.com/khalidt/Aegis/releases/download/v1.0.0/Aegis.zip'
+  url 'https://github.com/khalidt/Aegis/blob/main/test/Aegis.zip'
   name 'Aegis'
   desc 'Aegis is a modern macOS desktop application that provides secure end-to-end message encryption using a hybrid RSA-AES design.'
   homepage 'https://github.com/khalidt/Aegis'
